4b1a6aeb5bc55086658ff12c825139439c98080a
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-09-30 18:54:20 +0900

    Set up a PHP development environment

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -4,13 +4,22 @@ set -euxo pipefail
 
 PROJECT_PATH=/srv/ticket-sales.com
 
-# Nginxユーザーに以下のファイルにアクセスする権限を与える
+cd $PROJECT_PATH
+
+# Install Xdebug
+if [ "$APP_ENV" = "local" ]; then
+  pecl install xdebug-3.3.1
+  cp docker/web/php/conf.d/99-xdebug.ini /etc/php.d/99-xdebug.ini
+fi
+
+# Add permissions to certain files so that specific users, such as the Nginx user and root, can access them.
 chmod 777 "$PROJECT_PATH/storage/logs"
 chmod 777 "$PROJECT_PATH/storage/framework/views"
 chmod 777 "$PROJECT_PATH/database"
 chmod 777 "$PROJECT_PATH/database/database.sqlite"
+chmod 755 "$PROJECT_PATH/docker/web/start.sh"
 
-# php-fpmとNGINX起動
-# nginxは「-g "daemon off;"」でフォアグラウンド実行になり、コンテナが自動的に終了しなくなる
+# Start php-fpm and NGINX
+# By using -g "daemon off;", NGINX runs in the foreground, preventing the container from exiting automatically
 php-fpm
 nginx -g "daemon off;"
