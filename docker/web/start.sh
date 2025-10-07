32c4b3d6c2ce70367179c6b7e9c565dd1938316f
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-10-07 18:38:56 +0900

    Change the php-fpm executable user to the nginx user and add the nginx user to the root group

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -12,11 +12,8 @@ if [ "$APP_ENV" = "local" ]; then
   cp docker/web/php/conf.d/99-xdebug.ini /etc/php.d/99-xdebug.ini
 fi
 
-# Add permissions to certain files so that specific users, such as the Nginx user and root, can access them.
-chmod 777 "$PROJECT_PATH/storage/logs"
-chmod 777 "$PROJECT_PATH/storage/framework/views"
-chmod 777 "$PROJECT_PATH/database"
-chmod 777 "$PROJECT_PATH/database/database.sqlite"
+# Add the nginx user to the root group for permission access
+usermod -aG root nginx
 
 # Start php-fpm and NGINX
 # By using -g "daemon off;", NGINX runs in the foreground, preventing the container from exiting automatically
