ac25a0f841eee5b5f9b6fe5dd0c9008bd5e91584
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-10-21 15:43:25 +0900

    Add wayfinder

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { wayfinder } from "@laravel/vite-plugin-wayfinder";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
         tailwindcss(),
         react(),
+        wayfinder(),
     ],
     server: {
         host: true,
