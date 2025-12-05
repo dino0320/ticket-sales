cf094900251be82d09780d63cafc781beb0200f0
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-12-05 15:07:48 +0900

    Add valkey

--- docker/web/install-npm.sh
+++ docker/web/install-npm.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
