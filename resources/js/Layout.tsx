9ad516bb0a5d057152f06940ebe75216134564e9
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-12-11 11:23:04 +0900

    Modify header

--- resources/js/Layout.tsx
+++ resources/js/Layout.tsx
@@ -1,14 +1,16 @@
-import { index } from "@/actions/App/Http/Controllers/HomeController";
+import { index } from '@/actions/App/Http/Controllers/HomeController';
 import { Link } from '@inertiajs/react'
 import type { ReactNode } from 'react'
+import { MdAccountCircle } from 'react-icons/md';
+import { FaCartShopping } from 'react-icons/fa6';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <main>
       <header>
         <Link href={index()}>Home</Link>
-        <Link href="/my-account">My Account</Link>
-        <Link href="/cart">Cart</Link>
+        <Link href="my-account"><MdAccountCircle />My Account</Link>
+        <Link href="/cart"><FaCartShopping />Cart</Link>
       </header>
       <article>{children}</article>
     </main>
