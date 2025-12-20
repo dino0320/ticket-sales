63f096362c749c8dcfeb44b45e8c7aeb7adf04d5
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-12-20 12:11:27 +0900

    Fix links in Layout

--- resources/js/Layout.tsx
+++ resources/js/Layout.tsx
@@ -1,4 +1,6 @@
 import { index } from '@/actions/App/Http/Controllers/HomeController';
+import { show as showAccount } from '@/actions/App/Http/Controllers/AccountController';
+import { show as showCart } from '@/actions/App/Http/Controllers/CartController';
 import { Link } from '@inertiajs/react'
 import type { ReactNode } from 'react'
 import { MdAccountCircle } from 'react-icons/md';
@@ -9,8 +11,8 @@ export default function Layout({ children }: { children: ReactNode }) {
     <main>
       <header>
         <Link href={index()}>Home</Link>
-        <Link href="my-account"><MdAccountCircle />My Account</Link>
-        <Link href="/cart"><FaCartShopping />Cart</Link>
+        <Link href={showAccount()}><MdAccountCircle />My Account</Link>
+        <Link href={showCart()}><FaCartShopping />Cart</Link>
       </header>
       <article>{children}</article>
     </main>
